c438ded49ec9e51564385f92539c5a19ac5d80a0
chaizhixiang <zhixiag.chai@yiducloud.cn>
2020-03-20 19:12:00 +0800

    config

---
+++ ts-project/App.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class App extends React.Component<P, S> {
+    constructor(props: Readonly<P>){
+        super(props);
+    }
+    public render() {
+        return (
+            <div>1</div>
+        );
+    }
+}
+
+export default App;
